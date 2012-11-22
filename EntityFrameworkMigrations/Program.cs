f3e4ee642fa388c28e164ff2bea9d5386578cd29
Mitch Denny <mitch@mitchdenny.com>
2012-11-22 22:05:57 +1100

    Implemented dbmigrator sample code.
    Built out the program file with the option for running db migrator code,
    or initializer code. At the moment only the dbmigrator is written.

--- EntityFrameworkMigrations/Program.cs
+++ EntityFrameworkMigrations/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using EntityFrameworkMigrations.Migrations;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,75 @@ namespace EntityFrameworkMigrations
     {
         public static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                ShowUsage();
+                return;
+            }
+
+            var method = args[0];
+            var connectionString = args[1];
+
+            switch (method.ToLower())
+            {
+                case "dbmigrator":
+                    MigrateUsingDbMigrator(connectionString);
+                    break;
+
+                case "initializer":
+                    MigrateUsingInitializer(connectionString);
+                    break;
+
+                default:
+                    ShowUsage();
+                    return;
+            }
+        }
+
+        private static void MigrateUsingInitializer(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void MigrateUsingDbMigrator(string connectionString)
+        {
+            var connectionInfo = new DbConnectionInfo(connectionString, "System.Data.SqlClient");
+            var configuration = new Configuration();
+            configuration.TargetDatabase = connectionInfo;
+
+            var migrator = new DbMigrator(configuration);
+
+            Console.WriteLine("Before Migration");
+            foreach (var applied in migrator.GetDatabaseMigrations())
+            {
+                Console.WriteLine("\tApplied: {0}", applied);
+            }
+            foreach (var pending in migrator.GetPendingMigrations())
+            {
+                Console.WriteLine("\tPending: {0}", pending);
+            }
+            Console.WriteLine();
+
+            migrator.Update();
+
+            Console.WriteLine("After Migration");
+            foreach (var applied in migrator.GetDatabaseMigrations())
+            {
+                Console.WriteLine("\tApplied: {0}", applied);
+            }
+            foreach (var pending in migrator.GetPendingMigrations())
+            {
+                Console.WriteLine("\tPending: {0}", pending);
+            }
+            Console.WriteLine();
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Usage: EntityFrameworkMigrations.exe [method] [connection string]");
+            Console.WriteLine();
+            Console.WriteLine("\t[method] - dbmigrator, initializer");
+            Console.WriteLine("\t[connection string] - self explanitory");
         }
     }
 }
